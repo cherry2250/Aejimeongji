c221537ea2dd05b5f59aa8a048d0fb67b2536d3c
SSAFY <hehe2you@gmail.com>
2022-07-26 01:07:45 +0900

    front_feat : 온보딩 화면 (#10)

--- Front/AeJiMeongJi/App.js
+++ Front/AeJiMeongJi/App.js
@@ -6,7 +6,8 @@ import WelcomeScreen from './src/screens/Auth/WelcomScreen';
 import MainHome from './src/screens/Home/MainHome';
 import Initial from './src/screens/Initial';
 import Running from './src/screens/Running/Running';
-
+import LoginScreen from './src/screens/Auth/LoginScreen';
+import SignupScreen from './src/screens/Auth/SignupScreen';
 
 
 const Stack = createNativeStackNavigator();
@@ -22,6 +23,8 @@ const Navigation = () => {
         <Stack.Screen name="Welcome" component={WelcomeScreen}/>
         <Stack.Screen name="Running" component={Running}/>
         <Stack.Screen name="Home" component={MainHome}/>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Signup" component={SignupScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
