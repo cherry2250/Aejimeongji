95f0e1445a145d8a3404401e666d4f944051d2dd
SSAFY <hehe2you@gmail.com>
2022-07-25 09:28:21 +0900

    [FE] 초기 설정

---
+++ Front/AeJiMeongJi/App.js
@@ -0,0 +1,9 @@
+
+
+export default function App() {
+  return (
+    <>
+      <StatusBar backgroundColor="black" barStyle="light-content" />
+    </>
+  );
+}
---
+++ Front/AeJiMeongJi/constants/styles.js
@@ -0,0 +1,10 @@
+export const Colors = {
+  back100: '#FFF8EA',
+  back200: '#E9DBCA',
+  btnBack100: '#DD9944',
+  btnBack200: '#DDB244',
+  btnText: '#FBEDD3',
+  notification: '#98CB58',
+  inputBorder: '#D6D6D6',
+};
+
