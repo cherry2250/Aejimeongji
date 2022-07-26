dc8c1cb9435a5f28461f957e18f41c14d6f2b834
TobeyKwon <tobeykwon@gmail.com>
2022-07-26 15:07:04 +0900

    프로젝트 생성

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/AejimeongjiApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.aejimeongji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AejimeongjiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AejimeongjiApplication.class, args);
+	}
+
+}
