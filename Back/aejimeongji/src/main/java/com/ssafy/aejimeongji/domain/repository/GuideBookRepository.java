46cfa438142819c53acc920412c1d1b163a2869a
박지은 <skysun102@naver.com>
2022-07-29 22:46:56 +0900

    back_feature/가이드북도메인

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/repository/GuideBookRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.aejimeongji.domain.repository;
+
+
+import com.ssafy.aejimeongji.domain.entity.GuideBook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuideBookRepository extends JpaRepository<GuideBook, Long> {
+}
