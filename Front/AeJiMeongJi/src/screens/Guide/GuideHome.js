97e3e989abf0edfc001859d6838a8d850e9bb939
kimchaeri <kcr7812@naver.com>
2022-07-29 17:46:07 +0900

    feat : 가이드 홈 구현 (#17)

--- Front/AeJiMeongJi/src/screens/Guide/GuideHome.js
+++ Front/AeJiMeongJi/src/screens/Guide/GuideHome.js
@@ -17,20 +17,32 @@ const GuideHome = () => {
           <GuideButton style={{marginLeft: 10}}> 훈련 </GuideButton>
           <GuideButton style={{marginLeft: 10}}> 정보 </GuideButton>
         </View>
-        <View style={styles.GuideTitle}>
+        <View style={styles.guideTitle}>
           <Text style={{fontSize: 18, fontWeight: 'bold'}}>
             앵두님께 추천드리는 가이드
           </Text>
         </View>
-        <View style={{flex: 1.3}}>
-          <CarouselCards style={styles.CarouselCards} />
+        <View style={{flex: 4}}>
+          <CarouselCards style={styles.carouselCards} />
         </View>
 
         <View style={styles.subGuide}>
-          <Text>반려생활 가이드</Text>
-          <SubCard></SubCard>
-          <SubCard></SubCard>
-          <SubCard></SubCard>
+          <Text style={styles.subGuideTitle}>
+            지금 핫한 반려생활 가이드 Top5!
+          </Text>
+          <SubCard style={styles.subCards}></SubCard>
+          <SubCard style={styles.subCards}></SubCard>
+          <SubCard style={styles.subCards}></SubCard>
+          <SubCard style={styles.subCards}></SubCard>
+          <SubCard style={styles.subCards}></SubCard>
+        </View>
+        <View style={styles.subguideTitle2}>
+          <Text style={{fontSize: 18, fontWeight: 'bold'}}>
+            현재 연령대에 꼭 알아야 하는 정보
+          </Text>
+        </View>
+        <View style={{flex: 4}}>
+          <CarouselCards style={styles.carouselCards} />
         </View>
       </View>
     </ScrollView>
@@ -43,20 +55,39 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 30,
-    height: 3000,
+    height: 1900,
   },
-  GuideTitle: {
-    flex: 0.1,
+  guideTitle: {
+    flex: 0.4,
     marginBottom: 20,
     height: 200,
   },
-  CarouselCards: {},
+  subguideTitle2: {
+    flex: 0.38,
+    marginBottom: 20,
+  },
+  subGuideTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  carouselCards: {},
   subGuide: {
-    flex: 10,
-    backgroundColor: 'yellow',
+    flex: 10.2,
     height: 10,
+    width: 350,
+    marginTop: 40,
+  },
+  subGuide2: {
+    flex: 7,
+    height: 10,
+    width: 350,
+    marginTop: 40,
   },
   ButtonBox: {},
+  subCards: {
+    marginBottom: 20,
+  },
 });
 
 export default GuideHome;
