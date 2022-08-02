be53a14770eac7e48ed89baa9273a5aa2be6bdee
박지은 <skysun102@naver.com>
2022-08-02 15:07:45 +0900

    feat: 강아지 프로필 컨트롤러 개발 (#30)

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/api/DogApiController.java
@@ -0,0 +1,57 @@
+package com.ssafy.aejimeongji.api;
+
+import com.ssafy.aejimeongji.api.dto.ResponseDTO;
+import com.ssafy.aejimeongji.api.dto.dog.DogProfileResponse;
+import com.ssafy.aejimeongji.api.dto.dog.DogSaveRequest;
+import com.ssafy.aejimeongji.api.dto.dog.DogUpdateRequest;
+import com.ssafy.aejimeongji.domain.entity.Breed;
+import com.ssafy.aejimeongji.domain.entity.Dog;
+import com.ssafy.aejimeongji.domain.entity.DogImage;
+import com.ssafy.aejimeongji.domain.entity.Member;
+import com.ssafy.aejimeongji.domain.service.DogService;
+import com.ssafy.aejimeongji.domain.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/member/{memberId}/dog")
+@RequiredArgsConstructor
+public class DogApiController {
+    private final DogService dogService;
+    private final MemberService memberService;
+
+    @GetMapping("/{dogId}/profile")
+    public ResponseEntity<DogProfileResponse> getDog(@PathVariable("memberId") Long memberId, @PathVariable("dogId") Long dogId) {
+        log.info("강아지 프로필 {} 상세 정보 요청", dogId);
+        Dog dog = dogService.findDog(dogId);
+        return ResponseEntity.ok().body(new DogProfileResponse(dog));
+    }
+
+    @PostMapping("")
+    public ResponseEntity<ResponseDTO> saveDog(@PathVariable("memberId") Long memberId, @RequestBody DogSaveRequest request) {
+        log.info("강아지 프로필 등록 요청");
+        Member member = memberService.findMember(memberId);
+        Breed breed = dogService.findBreed(request.getBreed().getBreedName());
+        DogImage image = new DogImage("originTestName", "storeTestName");   // 이미지 아직 없음
+        Long savedId = dogService.saveDog(request.convertDog(member, breed, image));
+        return ResponseEntity.ok(new ResponseDTO("강아지 프로필 " + savedId + " 등록이 완료되었습니다."));
+    }
+
+    @PutMapping("/{dogId}")
+    public ResponseEntity<ResponseDTO> updateDog(@PathVariable("memberId") Long memberId, @PathVariable("dogId") Long dogId, @RequestBody DogUpdateRequest request) {
+        log.info("강아지 프로필 {} 수정 요청", dogId);
+        Breed breed = dogService.findBreed(request.getBreed().getBreedName());
+        Long updatedId = dogService.updateDog(dogId, request.getName(), request.getBirthdate(), request.getAdoptedDay(), breed);
+        return ResponseEntity.ok(new ResponseDTO("강아지 프로필 "+ updatedId + " 수정이 완료되었습니다."));
+    }
+
+    @DeleteMapping("/{dogId}")
+    public ResponseEntity<ResponseDTO> deleteDog(@PathVariable("memberId") Long memberId, @PathVariable("dogId") Long dogId) {
+        log.info("강아지 프로필 {} 삭제 요청", dogId);
+        dogService.deleteDog(dogId);
+        return ResponseEntity.ok(new ResponseDTO("강아지 프로필 "+ dogId + " 삭제가 완료되었습니다."));
+    }
+}
--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/service/DogService.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/service/DogService.java
@@ -2,7 +2,9 @@ package com.ssafy.aejimeongji.domain.service;
 
 import com.ssafy.aejimeongji.domain.entity.Breed;
 import com.ssafy.aejimeongji.domain.entity.Dog;
+import com.ssafy.aejimeongji.domain.repository.BreedRepository;
 import com.ssafy.aejimeongji.domain.repository.DogRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,18 +14,23 @@ import java.time.LocalDate;
 @Slf4j
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class DogService {
 
     private final DogRepository dogRepository;
+    private final BreedRepository breedRepository;
 
-    public DogService(DogRepository dogRepository) {
-        this.dogRepository = dogRepository;
+    // 견종 조회
+    public Breed findBreed(String breedName) {
+        return breedRepository.findBreedByBreedName(breedName);
     }
 
     // 강아지 프로필 상세 조회
     public Dog findDog(Long dogId) {
-        return dogRepository.findById(dogId)
+        Dog dog = dogRepository.findById(dogId)
                 .orElseThrow(() -> new IllegalArgumentException("조회하신 강아지가 존재하지 않습니다."));
+        dog.getBreed().getBreedName();
+        return dog;
     }
 
     // 강아지 프로필 등록
