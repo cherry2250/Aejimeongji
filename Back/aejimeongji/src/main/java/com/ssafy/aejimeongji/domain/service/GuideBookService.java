b9d0251d4980da4ed12bcf70971862922a8550dd
박지은 <skysun102@naver.com>
2022-08-03 23:42:03 +0900

    feat : 가이드 컨트롤러 수정 및 삭제 API 개발 (#19)

--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/service/GuideBookService.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/service/GuideBookService.java
@@ -2,6 +2,7 @@ package com.ssafy.aejimeongji.domain.service;
 
 import com.ssafy.aejimeongji.domain.entity.GuideBook;
 import com.ssafy.aejimeongji.domain.repository.GuideBookRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,13 +13,10 @@ import java.util.Optional;
 @Slf4j
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 public class GuideBookService {
 
     private final GuideBookRepository guideBookRepository;
-
-    public GuideBookService(GuideBookRepository guideBookRepository) {
-        this.guideBookRepository = guideBookRepository;
-    }
 
     // 강아지 맞춤형 가이드 목록 조회 - 전체 목록 조회 로직까지 구현됨
     public List<GuideBook> findGuideBookList() {
@@ -34,7 +32,21 @@ public class GuideBookService {
     // 가이드 생성
     @Transactional
     public Long saveGuideBook(GuideBook guideBook) {
-        System.out.println(guideBook);
         return guideBookRepository.save(guideBook).getId();
+    }
+
+    // 가이드 수정
+    @Transactional
+    public Long updateGuideBook(Long guideId, String newTitle, String newContent, String newCategory, int newDogAge, int newDogWeight) {
+        GuideBook findGuide = findGuideBook(guideId);
+        findGuide.updateGuideBook(newTitle, newContent, newCategory, newDogAge, newDogWeight);
+        return findGuide.getId();
+    }
+
+    // 가이드 삭제
+    @Transactional
+    public void deleteGuideBook(Long guidId) {
+        GuideBook findGuide = findGuideBook(guidId);
+        guideBookRepository.delete(findGuide);
     }
 }
