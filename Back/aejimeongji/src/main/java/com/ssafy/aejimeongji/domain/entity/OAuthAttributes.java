8c31a0b09fa7d7cec386bb08bdcd7c74b98c03dd
TobeyKwon <tobeykwon@gmail.com>
2022-08-08 01:14:07 +0900

    feat : OAuthAttributes 추가 (#54)

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/entity/OAuthAttributes.java
@@ -0,0 +1,37 @@
+package com.ssafy.aejimeongji.domain.entity;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+
+public enum OAuthAttributes {
+    GOOGLE("google", (attributes) -> {
+        return new UserProfile(
+                String.valueOf(attributes.get("sub")),
+                (String) attributes.get("email")
+        );
+    }),
+    NAVER("naver", (attributes) -> {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+        return new UserProfile(
+                (String) response.get("id"),
+                (String) response.get("email")
+        );
+    });
+
+    private final String registrationId;
+    private final Function<Map<String, Object>, UserProfile> of;
+
+    OAuthAttributes(String registrationId, Function<Map<String, Object>, UserProfile> of) {
+        this.registrationId = registrationId;
+        this.of = of;
+    }
+
+    public static UserProfile extract(String registrationId, Map<String, Object> attributes) {
+        return Arrays.stream(values())
+                .filter(provider -> registrationId.equals(provider.registrationId))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new)
+                .of.apply(attributes);
+    }
+}
