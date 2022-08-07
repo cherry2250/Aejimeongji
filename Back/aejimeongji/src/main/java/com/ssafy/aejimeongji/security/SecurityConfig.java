42c9494b9b683688c015e66ab33f892518c5faf8
TobeyKwon <tobeykwon@gmail.com>
2022-08-08 01:15:56 +0900

    feat : SecurityConfig OAuth 설정 추가 (#54)

--- Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/security/SecurityConfig.java
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/security/SecurityConfig.java
@@ -1,32 +1,49 @@
 package com.ssafy.aejimeongji.security;
 
 
+import com.ssafy.aejimeongji.domain.service.OAuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final OAuthService oAuthService;
+    private final OAuthAuthenicationSuccesHandler authenicationSuccesHandler;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .cors()
-                .configurationSource(corsConfigurationSource());
-
-        http
-                .csrf()
-                .disable();
-
-        http
+                .configurationSource(corsConfigurationSource())
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(oAuthService)
+                .and()
+                .successHandler(authenicationSuccesHandler)
+                .and()
                 .authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .formLogin().disable();
     }
 
     // PasswordEncoder 컨테이너 등록
@@ -50,4 +67,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
+
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.debug(true);
+//    }
 }
