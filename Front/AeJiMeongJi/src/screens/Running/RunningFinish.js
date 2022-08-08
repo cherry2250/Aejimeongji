eb5fa627f864479647390efc6917089496b51f03
kimchaeri <kcr7812@naver.com>
2022-08-08 17:38:53 +0900

    feat : 산책 중간 점검 (#35)

--- Front/AeJiMeongJi/src/screens/Running/RunningFinish.js
+++ Front/AeJiMeongJi/src/screens/Running/RunningFinish.js
@@ -11,3 +11,28 @@ const RunningFinish = () => {
 };
 
 export default RunningFinish;
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 25,
+    paddingVertical: 11,
+    paddingHorizontal: 28,
+    backgroundColor: Colors.btnBack100,
+    elevation: 2,
+    shadowColor: 'black',
+    shadowOffset: {width: 1, height: 1},
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: Colors.btnText,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
