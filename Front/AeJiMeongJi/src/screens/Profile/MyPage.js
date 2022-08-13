21dd6ff4bec86dc5ed4e54d4506c1857c1a07316
신혜원 <hehe2you@gmail.com>
2022-08-13 19:15:06 +0900

    feat : mypage 즐겨찾기 가이드 목록 back 연동 (#50)

--- Front/AeJiMeongJi/src/screens/Profile/MyPage.js
+++ Front/AeJiMeongJi/src/screens/Profile/MyPage.js
@@ -1,6 +1,7 @@
 import React, {useLayoutEffect, useState} from 'react';
-import {FlatList, StyleSheet, View} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {
+  responsiveFontSize,
   responsiveHeight,
   responsiveWidth,
 } from 'react-native-responsive-dimensions';
@@ -12,6 +13,44 @@ import NoGuide from '../../components/Profile/NoGuide';
 import {Colors} from '../../constants/styles';
 import {fetchLikedGuide, getDog} from '../../utils/profile';
 
+const DummyData = [
+  {
+    id: 1,
+    source:
+      'https://cdn.pixabay.com/photo/2018/07/14/15/27/cafe-3537801_960_720.jpg',
+  },
+  {
+    id: 2,
+    source:
+      'https://cdn.pixabay.com/photo/2017/04/10/22/28/residence-2219972_960_720.jpg',
+  },
+  {
+    id: 3,
+    source:
+      'https://cdn.pixabay.com/photo/2013/04/11/19/46/building-102840__340.jpg',
+  },
+  {
+    id: 4,
+    source:
+      'https://cdn.pixabay.com/photo/2015/11/17/18/59/architecture-1048092__340.jpg',
+  },
+  {
+    id: 5,
+    source:
+      'https://cdn.pixabay.com/photo/2017/03/05/00/34/panorama-2117310__340.jpg',
+  },
+  {
+    id: 6,
+    source:
+      'https://cdn.pixabay.com/photo/2017/10/17/19/11/fantasy-2861815__340.jpg',
+  },
+  {
+    id: 7,
+    source:
+      'https://cdn.pixabay.com/photo/2016/09/19/22/46/lake-1681485__340.jpg',
+  },
+];
+
 const MyPage = () => {
   const [guide, setGuide] = useState();
   const [source, setSource] = useState();
@@ -20,26 +59,33 @@ const MyPage = () => {
 
   useLayoutEffect(() => {
     const fetchInitialData = async () => {
-      // const res = await fetchLikedGuide();
+      const liked = await fetchLikedGuide();
 
       const res = await getDog(dogId);
-      // console.log(res);
       setDogName(res.name);
+      setGuide(liked.data);
       setSource(`http://i7d203.p.ssafy.io:8080/api/image/${res.imageName}`);
-      // console.log(res, 'guide');
     };
     fetchInitialData();
   }, []);
 
   return (
-    <View style={styles.rootContainer}>
+    <ScrollView style={styles.rootContainer}>
       <PlaceNavbar source={source}>MyPage</PlaceNavbar>
       <View style={styles.ConnectMyInfo}>
         <ConnectMyInfo dogName={dogName} />
       </View>
       <View style={styles.noGuideContainer}>{!guide && <NoGuide />}</View>
-      <View>{guide && <FlatList data={guide} renderItem={LikeGuide} />}</View>
-    </View>
+      <View>
+        <Text style={styles.likedTitle}> 즐겨찾기 한 가이드 목록 </Text>
+        <FlatList
+          contentContainerStyle={styles.guideContainer}
+          key={'#'}
+          data={DummyData}
+          renderItem={LikeGuide}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -54,9 +100,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  likedTitle: {
+    marginBottom: responsiveHeight(2),
+    marginLeft: responsiveWidth(4),
+    fontSize: responsiveFontSize(2.5),
+    fontWeight: 'bold',
+  },
   noGuideContainer: {
     position: 'absolute',
     top: responsiveHeight(40),
     left: responsiveWidth(10),
+  },
+  guideContainer: {
+    alignItems: 'center',
   },
 });
