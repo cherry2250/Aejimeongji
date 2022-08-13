651c22b569777b79b6100ea52a6606bde44206e7
박경열 <hrq789@naver.com>
2022-08-14 02:21:47 +0900

    feat : FCM 서비스 생성 (#73)

---
+++ Back/aejimeongji/src/main/java/com/ssafy/aejimeongji/domain/service/FcmService.java
@@ -0,0 +1,68 @@
+package com.ssafy.aejimeongji.domain.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.common.net.HttpHeaders;
+import com.ssafy.aejimeongji.api.dto.pushalert.FCMMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.*;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class FcmService {
+
+    private String getAccessToken() throws IOException {
+        String firebaseConfig = "src/main/resources/firbase/aejimeongji-ced81-firebase-adminsdk-d9fj5-6637e27afe.json";
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource(firebaseConfig).getInputStream())
+                .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+        googleCredentials.refreshIfExpired();
+        return googleCredentials.getAccessToken().getTokenValue();
+    }
+
+    private String API_URL = "https://fcm.googleapis.com/v1/projects/aejimeongji-ced81/messages:send";
+    private final ObjectMapper objectMapper;
+
+    public void sendMessageTo(String targetToken, String title, String body) throws IOException {
+        String message = makeMessage(targetToken, title, body);
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message, MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        log.info(response.body().string());
+    }
+
+    private String makeMessage(String targetToken, String title, String body) throws JsonProcessingException {
+        FCMMessage fcmMessage = FCMMessage.builder()
+                .message(FCMMessage.Message.builder()
+                        .token(targetToken)
+                        .notification(FCMMessage.Notification.builder()
+                                .title(title)
+                                .body(body)
+                                .image(null)
+                                .build()
+                        )
+                        .build()
+                )
+                .validate_only(false)
+                .build();
+        return objectMapper.writeValueAsString(fcmMessage);
+    }
+
+}
