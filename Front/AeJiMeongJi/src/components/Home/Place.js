1c4493dc72f17ea291113457fa9878a7523156b6
박지현 <spy03128@naver.com>
2022-08-16 23:40:40 +0900

    feat : 홈화면 산책데이터, 플레이스데이터, 예방접종 데이터 연동 (#61)

--- Front/AeJiMeongJi/src/components/Home/Place.js
+++ Front/AeJiMeongJi/src/components/Home/Place.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   Image,
   StyleSheet,
@@ -13,12 +13,25 @@ import {
   responsiveWidth,
   responsiveFontSize,
 } from 'react-native-responsive-dimensions';
+import axios from 'axios';
 
 import {Colors} from '../../constants/styles';
 import {useNavigation} from '@react-navigation/native';
+const url = 'http://i7d203.p.ssafy.io:8080';
 
 const Place = () => {
   const navigation = useNavigation();
+  const [bestPlace, setBestPlace] = useState({});
+
+  useEffect(() => {
+    axios.get(url + `/api/petplace/pop`).then(response => {
+      if (response.status == 200) {
+        setBestPlace(response.data);
+      } else {
+        console.log(error.response + '플레이스 가져오기 에러');
+      }
+    });
+  }, []);
 
   return (
     <View style={styles.box}>
@@ -42,85 +55,112 @@ const Place = () => {
           />
         </Pressable>
       </View>
-
-      <View style={styles.placebox}>
-        <Image
-          style={{
-            width: responsiveWidth(80),
-            height: responsiveHeight(25),
-            borderRadius: 20,
-          }}
-          resizeMode="cover"
-          source={require('../../Assets/image/강아지숲.png')}
-        />
-        <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-          <Text style={[styles.font, styles.font18, styles.line40]}>
-            강아지 숲
-            <Text style={[styles.font, styles.font12, styles.line40]}>
-              {'  '}테마파크
-            </Text>
-          </Text>
-          <Text style={{lineHeight: 40, marginRight: responsiveHeight(2)}}>
-            <Text style={{color: 'red'}}>★ </Text>4.3/5
-          </Text>
-        </View>
-      </View>
-      <View style={[styles.placebox, styles.placesubBox]}>
-        <View
-          style={[styles.placesubcontent, {marginRight: responsiveHeight(2)}]}>
+      {bestPlace[0] ? (
+        <View style={styles.placebox}>
           <Image
             style={{
-              width: responsiveWidth(37),
-              height: responsiveHeight(20),
+              width: responsiveWidth(80),
+              height: responsiveHeight(25),
               borderRadius: 20,
             }}
             resizeMode="cover"
-            source={require('../../Assets/image/마이무.png')}
-          />
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}>
-            <Text style={[styles.font, styles.font14, styles.line40]}>
-              마이무
-              <Text style={[styles.font, styles.font10, styles.line40]}>
-                {'  '}동물사료제조
-              </Text>
-            </Text>
-          </View>
-          <Text style={{lineHeight: 20, marginRight: responsiveHeight(2)}}>
-            <Text style={{color: 'red'}}>★ </Text>4.21/5
-          </Text>
-        </View>
-        <View
-          style={[styles.placesubcontent, {marginLeft: responsiveHeight(2)}]}>
-          <Image
-            style={{
-              width: responsiveWidth(37),
-              height: responsiveHeight(20),
-              borderRadius: 20,
+            source={{
+              uri: bestPlace[0].petplaceImageUrl[0],
             }}
-            resizeMode="cover"
-            source={require('../../Assets/image/쿨쿨펫.png')}
           />
-          <View
-            style={{
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}>
-            <Text style={[styles.font, styles.font14, styles.line40]}>
-              쿨쿨펫
-              <Text style={[styles.font, styles.font10, styles.line40]}>
-                {'  '}반려동물호텔
+          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+            <Text style={[styles.font, styles.font18, styles.line40]}>
+              {bestPlace[0].name}
+              <Text style={[styles.font, styles.font12, styles.line40]}>
+                {'  '}
+                {bestPlace[0].category}
               </Text>
             </Text>
+            <Text style={{lineHeight: 40, marginRight: responsiveHeight(2)}}>
+              <Text style={{color: 'red'}}>★ </Text>
+              {bestPlace[0].rating}
+            </Text>
           </View>
-          <Text style={{lineHeight: 20, marginRight: responsiveHeight(2)}}>
-            <Text style={{color: 'red'}}>★ </Text>4.8/5
-          </Text>
         </View>
-      </View>
+      ) : (
+        <View>
+          <Text></Text>
+        </View>
+      )}
+      {bestPlace[0] ? (
+        <View style={[styles.placebox, styles.placesubBox]}>
+          <View
+            style={[
+              styles.placesubcontent,
+              {marginRight: responsiveHeight(2)},
+            ]}>
+            <Image
+              style={{
+                width: responsiveWidth(37),
+                height: responsiveHeight(20),
+                borderRadius: 20,
+              }}
+              resizeMode="cover"
+              source={{uri: bestPlace[1].petplaceImageUrl[0]}}
+            />
+            <View
+              style={{
+                justifyContent: 'space-between',
+              }}>
+              <Text style={[styles.font, styles.font14, styles.line40]}>
+                {bestPlace[1].name}
+              </Text>
+              <Text style={[styles.font, styles.font10, styles.line40]}>
+                {bestPlace[1].category}
+              </Text>
+            </View>
+            <Text
+              style={{
+                lineHeight: 20,
+                alignSelf: 'flex-end',
+                marginRight: responsiveHeight(2),
+                marginTop: responsiveHeight(-4),
+              }}>
+              <Text style={{color: 'red'}}>★ </Text> {bestPlace[1].rating}
+            </Text>
+          </View>
+          <View
+            style={[styles.placesubcontent, {marginLeft: responsiveHeight(2)}]}>
+            <Image
+              style={{
+                width: responsiveWidth(37),
+                height: responsiveHeight(20),
+                borderRadius: 20,
+              }}
+              resizeMode="cover"
+              source={{uri: bestPlace[2].petplaceImageUrl[0]}}
+            />
+            <View
+              style={{
+                // flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}>
+              <Text style={[styles.font, styles.font14, styles.line40]}>
+                {bestPlace[2].name}
+              </Text>
+              <Text style={[styles.font, styles.font10, styles.line40]}>
+                {bestPlace[2].category}
+              </Text>
+            </View>
+            <Text
+              style={{
+                lineHeight: 20,
+                alignSelf: 'flex-end',
+                marginRight: responsiveHeight(2),
+                marginTop: responsiveHeight(-4),
+              }}>
+              <Text style={{color: 'red'}}>★ </Text> {bestPlace[2].rating}
+            </Text>
+          </View>
+        </View>
+      ) : (
+        <View></View>
+      )}
     </View>
   );
 };
